8178a8b431c6ecd111bb5241b861fe856727d406
gammus <gamilemustapha@gmail.com>
2019-05-23 14:21:09 +0100

    first portfolio

---
+++ public/HeaderComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { NavBar, NavbarBrand } from 'reactstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <>
+            <NavBar
+            </>
+
+        );
+    }
+
+}
---
+++ public/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import '/app.css';
+import registerServiceWorker from './registerServiceWorker';
+
