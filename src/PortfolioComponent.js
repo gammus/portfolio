ccbd92da0268e07fe9af8af6aad6ad632cda8739
gammus <gamilemustapha@gmail.com>
2019-05-30 17:20:15 +0100

    added spacer className

--- src/PortfolioComponent.js
+++ src/PortfolioComponent.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import TileRow from './TileRowComponent';
 import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css';
 
 
 export default class Portfolio extends Component {
     render() {
 
         return (
-    <Container>
-        <h2 className="text-center special-heading" >Portfolio</h2>      
+    <Container className='spacer'>
+        <h2 className="text-center special-heading " >Portfolio</h2>      
         <TileRow />
     </Container>
         )
